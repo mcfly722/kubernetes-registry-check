fcae5912cc3aca81d43fcdb22837a4835185a412
Anonymous <7221798@gmail.com>
2021-01-22 12:48:19 +0300

    checkRegistry handler

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"flag"
+	"encoding/base64"
 	"encoding/json"
-	
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
 type RegistryConnectionResultRecord struct {
-	Url         string
-	Message     string
-	Success     bool
+	Url     string
+	Message string
+	Success bool
 }
 
 func (record *RegistryConnectionResultRecord) toString() string {
@@ -47,7 +50,6 @@ func (checker *RegistryChecker) Destroy() {
 	close(checker.Done)
 }
 
-
 type k8s struct {
 	clientset kubernetes.Interface
 }
@@ -76,108 +78,133 @@ func getRegistries(k8s *k8s, namespace string) (map[string]*Registry, error) {
 	if err != nil {
 		return nil, err
 	}
-	
-	for _,secret :=range secrets.Items {
-	
+
+	for _, secret := range secrets.Items {
+
 		b64data, ok := secret.Data[".dockerconfigjson"]
 		if ok {
-			var auths RegistryAuths 
-			
+			var auths RegistryAuths
+
 			err := json.Unmarshal(b64data, &auths)
 			if err != nil {
 				fmt.Println("decoding %v error:", secret.ObjectMeta.Name, err)
 			} else {
-			
-				for key := range auths.Auths{
-					_, keyIsAlreadyExists :=registries[key]
-					
+
+				for key := range auths.Auths {
+					_, keyIsAlreadyExists := registries[key]
+
 					if !keyIsAlreadyExists {
-						registries[key] = &Registry {
-							Name    : secret.ObjectMeta.Name,
-							Url     : key,
+						registries[key] = &Registry{
+							Name:     secret.ObjectMeta.Name,
+							Url:      key,
 							UserName: auths.Auths[key].UserName,
 							Password: auths.Auths[key].Password,
 						}
 					}
-					
+
 				}
 			}
 		}
 	}
-	
 
 	return registries, nil
 }
 
-func newRegistryChecker(registry *Registry, intervalSec int, output chan RegistryConnectionResultRecord) (*RegistryChecker, error) {
+func checkRegistry(url string, userName string, password string) *RegistryConnectionResultRecord {
+
+	fullUrl := fmt.Sprintf("https://%v/v2/_catalog", url)
+
+	encodedCredentials := base64.URLEncoding.EncodeToString([]byte(fmt.Sprintf("%v,%v", userName, password)))
+
+	request, err := http.Get(fullUrl)
+	request.Header.Set("Authorization", fmt.Sprintf("Basic %v", encodedCredentials))
+
+	response := &RegistryConnectionResultRecord{
+		Url:     url,
+		Success: false,
+	}
+
+	if err != nil {
+		response.Message = err.Error()
+		return response
+	}
+
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		response.Message = err.Error()
+		return response
+	}
+
+	response.Message = string(body)
+	response.Success = true
+	return response
+}
+
+func newRegistryChecker(registry *Registry, intervalSec int, output chan RegistryConnectionResultRecord, checkRegistry func(url string, userName string, password string) *RegistryConnectionResultRecord) (*RegistryChecker, error) {
 
 	checker := RegistryChecker{
 		Done: make(chan struct{}),
 	}
-	
-	go func() {
-		working:
-			for {
-				select {
-					case <-checker.Done:
-						break working
-					default:
-					{
-						record := RegistryConnectionResultRecord{
-							Url    : registry.Url,
-							Message: registry.UserName,
-							Success: true,
-						}
-						
-						output <- record
-						
-						time.Sleep(time.Duration(intervalSec)*time.Second)
 
-					}
+	go func() {
+	working:
+		for {
+			select {
+			case <-checker.Done:
+				break working
+			default:
+				{
+					record := checkRegistry(registry.Url, registry.UserName, registry.Password)
+
+					output <- *record
+
+					time.Sleep(time.Duration(intervalSec) * time.Second)
+
 				}
 			}
+		}
 		fmt.Println(fmt.Sprintf("checker for '%v' registry has finished", registry.Name))
 	}()
-	
+
 	return &checker, nil
 }
 
-func newRegistryPool(k8s *k8s, namespace string, output chan RegistryConnectionResultRecord,configRefreshInterval time.Duration, checkIntervalSec int) {
+func newRegistryPool(k8s *k8s, namespace string, output chan RegistryConnectionResultRecord, configRefreshInterval time.Duration, checkIntervalSec int, checkRegistry func(url string, userName string, password string) *RegistryConnectionResultRecord) {
 
 	checkers := map[string]*RegistryChecker{}
-	
+
 	for {
 		registries, err := getRegistries(k8s, namespace)
 		if err != nil {
 			fmt.Println(fmt.Sprintf("error: %v", err))
 		} else {
-			
-			for key,registry := range registries {
-				_, registryIsAlreadyChecking :=checkers[key]
+
+			for key, registry := range registries {
+				_, registryIsAlreadyChecking := checkers[key]
 				if !registryIsAlreadyChecking {
 					// add new registry check
-					
-					checker,err := newRegistryChecker(registry, checkIntervalSec, output)
-					if err!=nil {
-						fmt.Println(fmt.Sprintf("error for %v: %v", registry.Name,err))
+
+					checker, err := newRegistryChecker(registry, checkIntervalSec, output, checkRegistry)
+					if err != nil {
+						fmt.Println(fmt.Sprintf("error for %v: %v", registry.Name, err))
 					} else {
 						checkers[key] = checker
-						fmt.Println(fmt.Sprintf("added registry check:%v (%v / user:%v)",registry.Name,registry.Url,registry.UserName))
+						fmt.Println(fmt.Sprintf("added registry check:%v (%v / user:%v)", registry.Name, registry.Url, registry.UserName))
 					}
 				}
 			}
 
-			for key,checker := range checkers {
-				_, checkerIsAlreadyExists :=registries[key]
+			for key, checker := range checkers {
+				_, checkerIsAlreadyExists := registries[key]
 				if !checkerIsAlreadyExists {
 					// deleting outdated registry checks
-					fmt.Println(fmt.Sprintf("deleted registry check: %v",key))
-					
+					fmt.Println(fmt.Sprintf("deleted registry check: %v", key))
+
 					checker.Destroy()
 					delete(checkers, key)
 				}
 			}
-		
+
 		}
 		time.Sleep(configRefreshInterval)
 	}
@@ -198,19 +225,16 @@ func main() {
 	fmt.Println(fmt.Sprintf("namespace = %s", *namespaceFlag))
 	fmt.Println(fmt.Sprintf("updateConfigIntervalSec = %v", *updateConfigSecFlag))
 
-
 	k8s, err := newK8s()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(fmt.Sprintf("Started"))
-	
-	
-	
+
 	output := make(chan RegistryConnectionResultRecord)
 
 	go func() {
-		newRegistryPool(k8s, *namespaceFlag, output, time.Duration(*updateConfigSecFlag)*time.Second, *checkIntervalSecFlag)
+		newRegistryPool(k8s, *namespaceFlag, output, time.Duration(*updateConfigSecFlag)*time.Second, *checkIntervalSecFlag, checkRegistry)
 	}()
 
 	// write to output all records
